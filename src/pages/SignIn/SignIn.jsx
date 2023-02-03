1da01a44546f6dd83eb1d8c908d3d678a8e3f723
bymine <frfr0723@gmail.com>
2023-02-03 16:29:32 +0900

    Inital project structure and files created

---
+++ src/pages/SignIn/SignIn.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./signIn.css";
+const SignIn = () => {
+  return <div>SignIn</div>;
+};
+
+export default SignIn;
---
+++ src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./signUp.css";
+const SignUp = () => {
+  return <div>SignUp</div>;
+};
+
+export default SignUp;
