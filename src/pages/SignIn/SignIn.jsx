d2f50f6735c26cd5502c5579985b164708cc2019
bymine <frfr0723@gmail.com>
2023-02-06 11:47:16 +0900

    feat: Implement SignIn and SignUp Components
    Implemented:
    - Validation of email and password on sign-in and sign-up pages
    - Enabled/disabled login and sign-up buttons based on email and password validation
    - Implemented password visibility/invisibility toggle
    
    Not Implemented:
    - Login and sign-up button request functions to server
    
    Added packages:
    - boxicons, axios

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,7 +1,111 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./signIn.css";
 const SignIn = () => {
-  return <div>SignIn</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [inputType, setInputType] = useState("password");
+
+  const emailInput = React.createRef();
+  const passwordInput = React.createRef();
+
+  useEffect(() => {
+    emailInput.current.addEventListener("keyup", (e) => {
+      setEmail(e.target.value);
+      checkEmail();
+    });
+
+    passwordInput.current.addEventListener("keyup", (e) => {
+      setPassword(e.target.value);
+      checkPassword();
+    });
+  });
+
+  function checkEmail() {
+    const emailRegex = /^[^ ]+@[^ ]+\.[a-z]/;
+    if (!email.match(emailRegex)) {
+      return setEmailError(true);
+    }
+    setEmailError(false);
+  }
+
+  function checkPassword() {
+    const passwordRegex = /[^ ]{8,}/;
+    if (!password.match(passwordRegex)) {
+      return setPasswordError(true);
+    }
+    setPasswordError(false);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  function showPassword() {
+    setInputType(inputType === "text" ? "password" : "text");
+  }
+
+  return (
+    <div className="signin">
+      <h1>Sign In</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="field">
+          <div className={`input-field ${emailError ? "invalid" : null}`}>
+            <input
+              ref={emailInput}
+              type="text"
+              placeholder="Enter your Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          {emailError && (
+            <span className="error">
+              <i className="bx bx-error-circle error-icon"></i>
+              <p className="error-text">Please enter a valid email</p>
+            </span>
+          )}
+        </div>
+        <div className="field">
+          <div className={`input-field ${passwordError ? "invalid" : null}`}>
+            <input
+              ref={passwordInput}
+              type={inputType}
+              placeholder="Enter your Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <i
+              className={`bx ${
+                inputType === "text" ? "bx-show" : "bx-hide"
+              } show-hide`}
+              onClick={showPassword}
+            ></i>
+          </div>
+          {passwordError && (
+            <span className="error">
+              <i className="bx bx-error-circle error-icon"></i>
+              <p className="error-text">Please enter at least 8 charatcer</p>
+            </span>
+          )}
+        </div>
+        <div className="input-field button">
+          <input
+            className="input-field button"
+            type="submit"
+            value="Sign in"
+            disabled={
+              emailError || passwordError || email === "" || password === ""
+            }
+          />
+        </div>
+
+        <p className="signup-link">
+          Don't have an account? <a href="/signup">SignUp</a>
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default SignIn;
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,7 +1,166 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./signUp.css";
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [email, setEmail] = useState("");
+  const [createPassword, setCreatePassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [emailError, setEmailError] = useState(false);
+  const [createPasswordError, setCreatePasswordError] = useState(false);
+  const [confirmPasswordError, setConfirmPasswordError] = useState(false);
+
+  const [createInputType, setCreateInputType] = useState("password");
+  const [confirmInputType, setConfirmInputType] = useState("password");
+
+  const emailInput = React.createRef();
+  const createPasswordInput = React.createRef();
+  const confirmPasswordInput = React.createRef();
+
+  useEffect(() => {
+    emailInput.current.addEventListener("keyup", (e) => {
+      setEmail(e.target.value);
+      checkEmail();
+    });
+
+    createPasswordInput.current.addEventListener("keyup", (e) => {
+      setCreatePassword(e.target.value);
+      checkPassword();
+      if (confirmPassword !== "") {
+        checkConfirmPassword();
+      }
+    });
+
+    confirmPasswordInput.current.addEventListener("keyup", (e) => {
+      setConfirmPassword(e.target.value);
+      checkConfirmPassword();
+    });
+  });
+
+  function checkEmail() {
+    const emailRegex = /^[^ ]+@[^ ]+\.[a-z]/;
+    if (!email.match(emailRegex)) {
+      return setEmailError(true);
+    }
+    setEmailError(false);
+  }
+
+  function checkPassword() {
+    const passwordRegex = /[^ ]{8,}/;
+    if (!createPassword.match(passwordRegex)) {
+      return setCreatePasswordError(true);
+    }
+    setCreatePasswordError(false);
+  }
+
+  function checkConfirmPassword() {
+    if (createPassword !== confirmPassword) {
+      return setConfirmPasswordError(true);
+    }
+    setConfirmPasswordError(false);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  function showPassword() {
+    setCreateInputType(createInputType === "text" ? "password" : "text");
+  }
+
+  function showConfrimPassword() {
+    setConfirmInputType(confirmInputType === "text" ? "password" : "text");
+  }
+
+  return (
+    <div className="signup">
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="field">
+          <div className={`input-field ${emailError ? "invalid" : null}`}>
+            <input
+              ref={emailInput}
+              type="text"
+              placeholder="Enter your Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          {emailError && (
+            <span className="error">
+              <i className="bx bx-error-circle error-icon"></i>
+              <p className="error-text">Please enter a valid email</p>
+            </span>
+          )}
+        </div>
+        <div className="field">
+          <div
+            className={`input-field ${createPasswordError ? "invalid" : null}`}
+          >
+            <input
+              ref={createPasswordInput}
+              type={createInputType}
+              placeholder="Enter your Create Password"
+              onChange={(e) => setCreatePassword(e.target.value)}
+            />
+            <i
+              className={`bx ${
+                createInputType === "text" ? "bx-show" : "bx-hide"
+              } show-hide`}
+              onClick={showPassword}
+            ></i>
+          </div>
+          {createPasswordError && (
+            <span className="error">
+              <i className="bx bx-error-circle error-icon"></i>
+              <p className="error-text">Please enter at least 8 charatcer</p>
+            </span>
+          )}
+        </div>
+        <div className="field">
+          <div
+            className={`input-field ${confirmPasswordError ? "invalid" : null}`}
+          >
+            <input
+              ref={confirmPasswordInput}
+              type={confirmInputType}
+              placeholder="Enter your Confirm Password"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            <i
+              className={`bx ${
+                confirmInputType === "text" ? "bx-show" : "bx-hide"
+              } show-hide`}
+              onClick={showConfrimPassword}
+            ></i>
+          </div>
+          {confirmPasswordError && (
+            <span className="error">
+              <i className="bx bx-error-circle error-icon"></i>
+              <p className="error-text">Please don't match</p>
+            </span>
+          )}
+        </div>
+        <div className="input-field button">
+          <input
+            className="input-field button"
+            type="submit"
+            value="Sign Up"
+            disabled={
+              emailError ||
+              createPasswordError ||
+              confirmPasswordError ||
+              email === "" ||
+              createPassword === "" ||
+              confirmPassword === ""
+            }
+          />
+        </div>
+
+        <p className="signin-link">
+          Already have an account? <a href="/signin">Signin</a>
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
