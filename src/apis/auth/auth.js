cc702301c12313c8624b8b4f8be4d47ea1ec63b2
bymine <frfr0723@gmail.com>
2023-02-21 21:10:00 +0900

    chore:  Change Server Port
    Change Server Port 5000 to 8000

--- src/apis/auth/auth.js
+++ src/apis/auth/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:5000/auth";
+const baseUrl = "http://localhost:8000/auth";
 
 export const postSignIn = async (email, password) => {
   try {
--- src/apis/todo/todo.js
+++ src/apis/todo/todo.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:5000/todos";
+const baseUrl = "http://localhost:8000/todos";
 
 const setHeaders = () => {
   return {
